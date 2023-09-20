d49d0e4db522b759f6d6b6fcdc902da65c3e569c
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-20 17:03:31 -0300

    Requisito 1 funcionando

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,7 +1,17 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de atividades para cadastrar: ");
+    String input1 = scanner.next();
+    int quantity = Integer.parseInt(input1);
+
+    for (int i = 1; i <= quantity; i++) {
+      System.out.print("Digite o nome da atividade " + i + ": ");
+      String input2 = scanner.next();
+    }
   }
 }
