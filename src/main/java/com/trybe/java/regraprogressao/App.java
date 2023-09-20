e54fa1952fea5cca33403b3775e1118b57e910c8
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-20 17:53:24 -0300

    Requisitos 1 e 2 ok

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -4,27 +4,24 @@ import java.util.Scanner;
 
 public class App {
 
-  /**
-   * Metodo main.
-   */
-
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Digite a quantidade de atividades para cadastrar: ");
-    String input1 = scanner.next();
-    int quantity = Integer.parseInt(input1);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int quantity = Integer.parseInt(scanner.nextLine());
     double weightSum = 0;
+    double totalAverage = 0;
 
     for (int i = 1; i <= quantity; i++) {
-      System.out.print("Digite o nome da atividade " + i + ": ");
-      String activityName = scanner.next();
+      System.out.println("Digite o nome da atividade " + i + ": ");
+      String activityName = scanner.nextLine();
 
-      System.out.print("Digite o peso da atividade " + i + ": ");
-      double weight = Double.parseDouble(scanner.next());
+      System.out.println("Digite o peso da atividade " + i + ": ");
+      int weight = Integer.parseInt(scanner.nextLine());
       weightSum += weight;
 
-      System.out.print("Digite a nota obtida para " + activityName + ": ");
-      double grade = Double.parseDouble(scanner.next());
+      System.out.println("Digite a nota obtida para " + activityName + ": ");
+      int grade = Integer.parseInt(scanner.nextLine());
+
     }
     System.out.print("Soma: " + weightSum);
   }
