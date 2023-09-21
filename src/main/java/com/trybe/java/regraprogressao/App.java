8a85c3d0b215df7b11c643ce6b7f0902eb3a5d2a
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-21 14:20:42 -0300

    feat: requisito 3 ok

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -14,7 +14,7 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int quantity = Integer.parseInt(scanner.nextLine());
     int weightSum = 0;
-    int totalAverage = 0;
+    int totalGrade = 0;
 
     for (int i = 1; i <= quantity; i++) {
       System.out.println("Digite o nome da atividade " + i + ": ");
@@ -26,8 +26,21 @@ public class App {
 
       System.out.println("Digite a nota obtida para " + activityName + ": ");
       int grade = Integer.parseInt(scanner.nextLine());
-
+      totalGrade += weight * grade;
     }
-    System.out.print("Soma: " + weightSum);
+    int avarage = totalGrade / 100;
+
+    if (weightSum != 100) {
+      System.out.println("O peso total precisa ser 100, tente novamente.");
+    } else {
+      if (avarage < 85) {
+        System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste "
+            + "período, " + avarage + "%, você não atingiu a "
+            + "pontuação mínima necessária para sua aprovação.");
+      } else {
+        System.out.println("Parabéns! Você alcançou " + avarage + "%! "
+            + "E temos o prazer de informar que você obteve aprovação! ");
+      }
+    }
   }
 }
